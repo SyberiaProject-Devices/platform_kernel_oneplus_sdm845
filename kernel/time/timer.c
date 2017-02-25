f1d22608cfc7dfa6277a15c85f2050f95c5f8c6f
Kyle Yan <kyan@codeaurora.org>
2017-02-24 16:49:27 -0800

    timer: Add a global deferrable timer
    Add a global deferrable timer in addition to the per-cpu deferrable timer
    to allow deferrable timers without the TIMER_PINNED flag to run
    on any active CPU.
    
    [rishabhb@codeaurora.org: Resolved conflicts in kernel/time/timer.c]
    Change-Id: I8e6b77cef972589912ad18f324c46c936fbbb96f
    Signed-off-by: Kyle Yan <kyan@codeaurora.org>
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -831,8 +831,11 @@ static inline struct timer_base *get_timer_cpu_base(u32 tflags, u32 cpu)
 	 * If the timer is deferrable and NO_HZ_COMMON is set then we need
 	 * to use the deferrable base.
 	 */
-	if (IS_ENABLED(CONFIG_NO_HZ_COMMON) && (tflags & TIMER_DEFERRABLE))
-		base = per_cpu_ptr(&timer_bases[BASE_DEF], cpu);
+	if (IS_ENABLED(CONFIG_NO_HZ_COMMON) && (tflags & TIMER_DEFERRABLE)) {
+		base = &timer_base_deferrable;
+		if (tflags & TIMER_PINNED)
+			base = per_cpu_ptr(&timer_bases[BASE_DEF], cpu);
+	}
 	return base;
 }
 
@@ -844,8 +847,11 @@ static inline struct timer_base *get_timer_this_cpu_base(u32 tflags)
 	 * If the timer is deferrable and NO_HZ_COMMON is set then we need
 	 * to use the deferrable base.
 	 */
-	if (IS_ENABLED(CONFIG_NO_HZ_COMMON) && (tflags & TIMER_DEFERRABLE))
-		base = this_cpu_ptr(&timer_bases[BASE_DEF]);
+	if (IS_ENABLED(CONFIG_NO_HZ_COMMON) && (tflags & TIMER_DEFERRABLE)) {
+		base = &timer_base_deferrable;
+		if (tflags & TIMER_PINNED)
+			base = this_cpu_ptr(&timer_bases[BASE_DEF]);
+	}
 	return base;
 }
 
@@ -1724,13 +1730,15 @@ static __latent_entropy void run_timer_softirq(struct softirq_action *h)
 	struct timer_base *base = this_cpu_ptr(&timer_bases[BASE_STD]);
 
 	__run_timers(base);
-	if (IS_ENABLED(CONFIG_NO_HZ_COMMON))
+	if (IS_ENABLED(CONFIG_NO_HZ_COMMON)) {
+		__run_timers(&timer_base_deferrable);
 		__run_timers(this_cpu_ptr(&timer_bases[BASE_DEF]));
 
 	if ((atomic_cmpxchg(&deferrable_pending, 1, 0) &&
 		tick_do_timer_cpu == TICK_DO_TIMER_NONE) ||
 		tick_do_timer_cpu == smp_processor_id())
 		__run_timers(&timer_base_deferrable);
+	}
 }
 
 /*
