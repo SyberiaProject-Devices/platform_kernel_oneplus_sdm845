3847dd513da2b0aea680021a6f180ff91e70fe73
Kristof Petho <kristof.petho@gmail.com>
2018-05-19 17:41:10 -0700

    treewide: fix GCC 8.1 warnings
    With commits from @SultanXDA and @nathanchance
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I5c04e9bc6a22a30c154f7d29f9f5fb7e64f1c18c
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -705,15 +705,15 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	if (panel->is_hbm_enabled){
 		return 0;
 		}
-    if (panel->bl_config.bl_high2bit){
-	if(HBM_flag==true){
-		return 0;
-		}
-	else{
-		rc = mipi_dsi_dcs_set_display_brightness_samsung(dsi, bl_lvl);
-		}
-    } else
-	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
+	if (panel->bl_config.bl_high2bit){
+		if(HBM_flag==true){
+			return 0;
+			}
+		else{
+			rc = mipi_dsi_dcs_set_display_brightness_samsung(dsi, bl_lvl);
+			}
+	} else
+		rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
 
