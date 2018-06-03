117a8b3c71387bad56f62fc9efad2ff3a09bf001
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-03 10:47:51 -0700

    ANDROID: Fix massive cpufreq_times memory leaks
    Every time _cpu_up() is called for a CPU, idle_thread_get() is called
    which then re-initializes a CPU's idle thread that was already
    previously created and cached in a global variable in
    smpboot.c. idle_thread_get() calls init_idle() which then calls
    __sched_fork(). __sched_fork() is where cpufreq_task_times_init() is,
    and cpufreq_task_times_init() allocates memory for the task struct's
    time_in_state array.
    
    Since idle_thread_get() reuses a task struct instance that was already
    previously created, this means that every time it calls init_idle(),
    cpufreq_task_times_init() allocates this array again and overwrites
    the existing allocation that the idle thread already had.
    
    This causes memory to be leaked every time a CPU is onlined. In order
    to fix this, move allocation of time_in_state into _do_fork to avoid
    allocating it at all for idle threads. The cpufreq times interface is
    intended to be used for tracking userspace tasks, so we can safely
    remove it from the kernel's idle threads without killing any
    functionality.
    
    But that's not all!
    
    Task structs can be freed outside of release_task(), which creates
    another memory leak because a task struct can be freed without having
    its cpufreq times allocation freed. To fix this, free the cpufreq
    times allocation at the same time that task struct allocations are
    freed, in free_task().
    
    Since free_task() can also be called in error paths of copy_process()
    after dup_task_struct(), set time_in_state to NULL immediately after
    calling dup_task_struct() to avoid possible double free.
    
    Bug description and fix adapted from patch submitted by
    Sultan Alsawaf <sultanxda@gmail.com> at
    https://android-review.googlesource.com/c/kernel/msm/+/700134
    
    Bug: 110044919
    Test: Hikey960 builds, boots & reports /proc/<pid>/time_in_state
    correctly
    Change-Id: I12fe7611fc88eb7f6c39f8f7629ad27b6ec4722c
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -77,6 +77,7 @@
 #include <linux/compiler.h>
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
+#include <linux/cpufreq_times.h>
 
 #include <linux/cpufreq.h>
 #include <linux/cpu_input_boost.h>
@@ -345,6 +346,8 @@ void put_task_stack(struct task_struct *tsk)
 
 void free_task(struct task_struct *tsk)
 {
+	cpufreq_task_times_exit(tsk);
+
 #ifndef CONFIG_THREAD_INFO_IN_TASK
 	/*
 	 * The task is finally done with both the stack and thread_info,
@@ -1554,6 +1557,8 @@ static __latent_entropy struct task_struct *copy_process(
 	 */
 	p->clear_child_tid = (clone_flags & CLONE_CHILD_CLEARTID) ? child_tidptr : NULL;
 
+	cpufreq_task_times_init(p);
+
 	ftrace_graph_init_task(p);
 
 	rt_mutex_init_task(p);
@@ -1989,6 +1994,8 @@ long _do_fork(unsigned long clone_flags,
 	if (!IS_ERR(p)) {
 		struct completion vfork;
 		struct pid *pid;
+
+		cpufreq_task_times_alloc(p);
 
 		trace_sched_process_fork(current, p);
 
