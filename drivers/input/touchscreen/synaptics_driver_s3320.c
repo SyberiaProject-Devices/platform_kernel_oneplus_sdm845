7e0b103eec903b31523f2fa98e79933af0639206
DennySPB <dennyspb@gmail.com>
2019-01-10 14:46:48 +0300

    Revert "s3320: require low latency"
    This reverts commit 460a423f671b5856c91e47ec920998d9a75b93c6.

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -24,7 +24,6 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/hrtimer.h>
-#include <linux/pm_qos.h>
 #include <linux/proc_fs.h>
 #include <linux/interrupt.h>
 #include <linux/regulator/consumer.h>
@@ -571,8 +570,6 @@ struct synaptics_ts_data {
 	unsigned int fp_aod_cnt;
 	unsigned int unlock_succes;
 	int project_version;
-
-	struct pm_qos_request pm_qos_req;
 };
 
 static struct device_attribute attrs_oem[] = {
@@ -1994,10 +1991,6 @@ static void synaptics_ts_work_func(struct work_struct *work)
 	if( ts->enable_remote) {
 		goto END;
 	}
-
-	/* prevent CPU from entering deep sleep */
-	pm_qos_update_request(&ts->pm_qos_req, 100);
-
 	ret = synaptics_rmi4_i2c_write_byte(ts->client, 0xff, 0x00 );
 	ret = synaptics_rmi4_i2c_read_word(ts->client, F01_RMI_DATA_BASE);
 
@@ -2069,8 +2062,6 @@ static void synaptics_ts_work_func(struct work_struct *work)
 
 
 END:
-	pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
-
 	//ret = set_changer_bit(ts);
 	touch_enable(ts);
 EXIT:
@@ -6308,10 +6299,6 @@ static int synaptics_ts_probe(struct i2c_client *client, const struct i2c_device
 		pr_warn("%s: sysfs_create_file failed for wake_gestures\n", __func__);
 	}
 #endif
-
-	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
-		PM_QOS_DEFAULT_VALUE);
-
 	TPDTM_DMESG("synaptics_ts_probe 3203: normal end\n");
 
 	bootmode = get_boot_mode();
@@ -6387,9 +6374,6 @@ static int synaptics_ts_remove(struct i2c_client *client)
 	input_free_device(ts->input_dev);
 	kfree(ts);
 	tpd_power(ts,0);
-
-	pm_qos_remove_request(&ts->pm_qos_req);
-
 	return 0;
 }
 
