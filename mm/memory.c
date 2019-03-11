a26909fa9770352ac11146600b351a12f2a2c819
Minchan Kim <minchan@google.com>
2019-03-11 11:42:15 +0900

    Revert "mm: VMA sequence count"
    This reverts commit 184ccc18de5fca9d7e40c093cfba316af121c4f7.
    
    Bug: 128240262
    Change-Id: I7293939d08e6b9603d82464f4538a573b3407a88
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1312,7 +1312,6 @@ void unmap_page_range(struct mmu_gather *tlb,
 	unsigned long next;
 
 	BUG_ON(addr >= end);
-	vm_write_begin(vma);
 	tlb_start_vma(tlb, vma);
 	pgd = pgd_offset(vma->vm_mm, addr);
 	do {
@@ -1322,7 +1321,6 @@ void unmap_page_range(struct mmu_gather *tlb,
 		next = zap_pud_range(tlb, vma, pgd, addr, next, details);
 	} while (pgd++, addr = next, addr != end);
 	tlb_end_vma(tlb, vma);
-	vm_write_end(vma);
 }
 
 
