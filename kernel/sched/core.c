MotorolaMobilityLLC/kernel-msm

sched: Improve the scheduler
This change is for general scheduler improvement.

Change-Id: Ib93a77c1d12574354213a37b1171a462b7cab685
Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

kernel/sched/core.c
@@ -783,6 +783,10 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 		sched_info_dequeued(rq, p);
 
 	p->sched_class->dequeue_task(rq, p, flags);
+#ifdef CONFIG_SCHED_WALT
+	if (p == rq->ed_task)
+		early_detection_notify(rq, ktime_get_ns());
+#endif
 	trace_sched_enq_deq_task(p, 0, cpumask_bits(&p->cpus_allowed)[0]);
 }
 

SyberiaProject-Devices/platform_kernel_oneplus_sdm845

Signed-off-by: Danny Lin <danny@kdrag0n.dev>
Signed-off-by: DennySPB <dennyspb@gmail.com>

kernel/sched/core.c
@@ -777,6 +777,10 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 		psi_dequeue(p, flags & DEQUEUE_SLEEP);
 	}
 	p->sched_class->dequeue_task(rq, p, flags);
+#ifdef CONFIG_SCHED_WALT
+	if (p == rq->ed_task)
+		early_detection_notify(rq, sched_ktime_clock());
+#endif
 	trace_sched_enq_deq_task(p, 0, cpumask_bits(&p->cpus_allowed)[0]);
 }
 