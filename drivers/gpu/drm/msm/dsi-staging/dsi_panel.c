767191c56aa340f161aa04c9c1b4be4617808277
DennySPB <dennyspb@gmail.com>
2019-05-14 17:28:40 +0300

    dsi-staging: exposure_adjustment: remove OP panel modes check
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Iaf033ed22eb4177371048746384587559bf8d07d

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -575,18 +575,14 @@ static int dsi_panel_tx_cmd_set(struct dsi_panel *panel,
 		return 0;
 
 #ifdef CONFIG_EXPOSURE_ADJUSTMENT
-	if (ea_panel_on()){
-		if (type == DSI_CMD_SET_SRGB_ON ||
-			type == DSI_CMD_SET_DCI_P3_ON ||
-			type == DSI_CMD_SET_ONEPLUS_MODE_ON)
-			ea_panel_mode_ctrl(panel, true);
-		else if ((type > DSI_CMD_SET_POST_TIMING_SWITCH &&
-			type < DSI_CMD_SET_PANEL_SERIAL_NUMBER) ||
-			type > DSI_CMD_READ_SAMSUNG_PANEL_REGISTER_OFF ||
-			type < DSI_CMD_SET_CMD_TO_VID_SWITCH)
-			ea_panel_mode_ctrl(panel, false);
+	if (ea_panel_on()) {
+	    if (type == DSI_CMD_SET_ON)
+		ea_panel_mode_ctrl(panel, true);
+	    else if (type == DSI_CMD_SET_OFF)
+		ea_panel_mode_ctrl(panel, false);
 	}
 #endif
+
 	mode = panel->cur_mode;
 
 	cmds = mode->priv_info->cmd_sets[type].cmds;
