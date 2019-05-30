858fdb0ef060f2fb105a1a7579c0cb0de99043af
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-29 23:15:09 -0700

    Revert "kernel: Boost to the max for a short amount of time when zygote forks"
    This isn't needed with our current performance setup.
    
    This reverts commit fd6cbb0.
    
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Idff4bac644c189df5facb8db7d111530112bd3ab

--- kernel/fork.c
+++ kernel/fork.c
@@ -78,8 +78,6 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
@@ -1977,14 +1975,6 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-#ifdef CONFIG_CPU_INPUT_BOOST
-	/* Boost CPU to the max for 32 ms when userspace launches an app */
-	if (task_is_zygote(current) && cpu_input_boost_within_input(75)) {
-		cpu_input_boost_kick_max(32);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
-	}
-#endif
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
