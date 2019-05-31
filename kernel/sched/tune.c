0e557d2bdd20284cf1383ca032416781d0e3d49a
ethan-halsall <ethan.halsall@gmail.com>
2019-05-31 09:54:43 +0100

    Revert "stune: increase DSB slots count to 6"
    This reverts commit 3c77eb74d3231e531602ad104e9f79f62b9c06ef.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I23538ce9fac4c833e7d936fadc17081152042d3a

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -24,7 +24,7 @@ extern struct reciprocal_value schedtune_spc_rdiv;
 struct target_nrg schedtune_target_nrg;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-#define DYNAMIC_BOOST_SLOTS_COUNT 6
+#define DYNAMIC_BOOST_SLOTS_COUNT 5
 static DEFINE_MUTEX(boost_slot_mutex);
 static DEFINE_MUTEX(stune_boost_mutex);
 struct boost_slot {
