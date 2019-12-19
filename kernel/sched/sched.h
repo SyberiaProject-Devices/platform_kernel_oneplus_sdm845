519d0f2fa39921cf2f3dd45748dc969e820c850d
Steven Rostedt (VMware) <rostedt@goodmis.org>
2019-12-19 16:44:54 -0500

    sched: Remove struct sched_class::next field
    Now that the sched_class descriptors are defined in order via the linker
    script vmlinux.lds.h, there's no reason to have a "next" pointer to the
    previous priroity structure. The order of the sturctures can be aligned as
    an array, and used to index and find the next sched_class descriptor.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20191219214558.845353593@goodmis.org
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1616,7 +1616,6 @@ extern const u32 sched_prio_to_wmult[40];
 #define RETRY_TASK		((void *)-1UL)
 
 struct sched_class {
-	const struct sched_class *next;
 
 	void (*enqueue_task) (struct rq *rq, struct task_struct *p, int flags);
 	void (*dequeue_task) (struct rq *rq, struct task_struct *p, int flags);
