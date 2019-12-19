369aeccff29c5a5d0ab76f25980a5b9c7f7ece27
Steven Rostedt (VMware) <rostedt@goodmis.org>
2019-12-19 16:44:53 -0500

    sched: Have sched_class_highest define by vmlinux.lds.h
    Now that the sched_class descriptors are defined by the linker script, and
    this needs to be aware of the existance of stop_sched_class when SMP is
    enabled or not, as it is used as the "highest" priority when defined. Move
    the declaration of sched_class_highest to the same location in the linker
    script that inserts stop_sched_class, and this will also make it easier to
    see what should be defined as the highest class, as this linker script
    location defines the priorities as well.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20191219214558.682913590@goodmis.org
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6243,6 +6243,14 @@ void __init sched_init(void)
 	unsigned long alloc_size = 0, ptr;
 
 	sched_clock_init();
+	/* Make sure the linker didn't screw up */
+	BUG_ON(&idle_sched_class + 1 != &fair_sched_class ||
+	       &fair_sched_class + 1 != &rt_sched_class ||
+	       &rt_sched_class + 1   != &dl_sched_class);
+#ifdef CONFIG_SMP
+	BUG_ON(&dl_sched_class + 1 != &stop_sched_class);
+#endif
+
 	wait_bit_init();
 
 
