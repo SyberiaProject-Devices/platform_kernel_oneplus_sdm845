60e7b6ecf3cf919a49f57006b5399d85d4ea5bbb
Lukasz Luba <lukasz.luba@arm.com>
2020-05-27 10:58:52 +0100

    PM / EM: change name of em_pd_energy to em_cpu_energy
    Energy Model framework now supports other devices than CPUs. Refactor some
    of the functions in order to prevent wrong usage. The old function
    em_pd_energy has to generic name. It must not be used without proper
    cpumask pointer, which is possible only for CPU devices. Thus, rename it
    and add proper description to warn of potential wrong usage for other
    devices.
    
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Acked-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6910,7 +6910,7 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 	}
 
 	if (!energy)
-		energy = em_pd_energy(pd->em_pd, max_util, sum_util, _cpu_cap);
+		energy = em_cpu_energy(pd->em_pd, max_util, sum_util, _cpu_cap);
 
 	return energy;
 }
