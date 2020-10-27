ce51cf9b6e8420fe77829b087a1b971395c88554
DennySPb <dennyspb@gmail.com>
2020-10-27 10:59:34 +0300

    Revert "mm: protect VMA modifications using VMA sequence count"
    This reverts commit dd2b4655c6cd361db3e3816a4929c94b1cd994d0.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -641,8 +641,7 @@ static int userfaultfd_release(struct inode *inode, struct file *file)
 			else
 				prev = vma;
 		}
-		vm_write_begin(vma);
-		WRITE_ONCE(vma->vm_flags, new_flags);
+		vma->vm_flags = new_flags;
 		vma->vm_userfaultfd_ctx = NULL_VM_UFFD_CTX;
 	}
 	up_write(&mm->mmap_sem);
