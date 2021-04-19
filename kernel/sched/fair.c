3d4c34fc39aa068ce6deedd9378ced43ad70f6cb
DennySPb <dennyspb@gmail.com>
2021-04-19 11:22:19 +0300

    Revert "sched/fair: Don't let tasks slip away from gold to silver cluster"
    This reverts commit e1e6dc54cf15985b30cbb4ffd094b1008274694e.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -150,8 +150,6 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  */
 unsigned int capacity_margin				= 1280;
 
-unsigned int sched_small_task_threshold = 102;
-
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
 {
 	lw->weight += inc;
@@ -9779,9 +9777,7 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 */
 		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
 		    capacity_of(env->dst_cpu) < capacity &&
-		    (rq->nr_running == 1 ||
-			 (rq->nr_running == 2 && task_util(rq->curr) <
-			  sched_small_task_threshold)))
+		    nr_running == 1)
 			continue;
 
 		switch (env->migration_type) {
