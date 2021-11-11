cce16ecd9185215b06ed75e07f38f4462d109460
DennySPb <dennyspb@gmail.com>
2021-11-11 11:29:43 +0300

    sched/stats: fix building with schedstats enabled
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/stats.h
+++ kernel/sched/stats.h
@@ -233,7 +233,7 @@ static inline void sched_info_depart(struct rq *rq, struct task_struct *t)
 	rq_sched_info_depart(rq, delta);
 
 	if (task_is_running(t))
-		sched_info_enqueue(rq, t);
+		sched_info_queued(rq, t);
 }
 
 /*
