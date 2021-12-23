9bf2dfce7e81491a903adb8a1437c76228c7b5b3
DennySPb <dennyspb@gmail.com>
2021-12-23 10:34:43 +0300

    Revert "Reland "sched/rt: Pull google's changes from gs101""
        * Will be properly implemented in vendor_sched hooks
    
    This reverts commit 9203b1954d9126703655cddacf39e2b765ee5712.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -5,22 +5,8 @@
  */
 #include "sched.h"
 #include "pelt.h"
-#include <linux/cpuidle.h>
 
 #include <trace/hooks/sched.h>
-
-#define DEFAULT_IMPRATANCE_THRESHOLD	1024
-#define DEF_UTIL_THRESHOLD  1280
-#define CPU_NUM             8
-
-unsigned int sched_capacity_margin[CPU_NUM] = {
-			[0 ... CPU_NUM-1] = DEF_UTIL_THRESHOLD};
-
-extern int cpu_is_idle(int cpu);
-extern int sched_cpu_idle(int cpu);
-
-#define cpu_overutilized(cap, max, cpu)	\
-		((cap) * sched_capacity_margin[cpu] > (max) << SCHED_CAPACITY_SHIFT)
 
 int sched_rr_timeslice = RR_TIMESLICE;
 int sysctl_sched_rr_timeslice = (MSEC_PER_SEC / HZ) * RR_TIMESLICE;
@@ -1449,16 +1435,18 @@ static inline bool rt_task_fits_capacity(struct task_struct *p, int cpu)
 	unsigned int min_cap;
 	unsigned int max_cap;
 	unsigned int cpu_cap;
-	struct rq *rq = cpu_rq(cpu);
+
+	/* Only heterogeneous systems can benefit from this check */
+	if (!static_branch_unlikely(&sched_asym_cpucapacity))
+		return true;
 
 	min_cap = uclamp_eff_value(p, UCLAMP_MIN);
 	max_cap = uclamp_eff_value(p, UCLAMP_MAX);
 
-	cpu_cap = capacity_orig_of(cpu) - thermal_load_avg(rq);
+	cpu_cap = capacity_orig_of(cpu);
 
 	return cpu_cap >= min(min_cap, max_cap);
 }
-
 #else
 static inline bool rt_task_fits_capacity(struct task_struct *p, int cpu)
 {
@@ -1468,7 +1456,6 @@ static inline bool rt_task_fits_capacity(struct task_struct *p, int cpu)
 
 #ifdef CONFIG_SMP
 static int find_lowest_rq(struct task_struct *task);
-static int find_lowest_rq_pixel(struct task_struct *p, struct cpumask *backup_mask);
 
 #ifdef CONFIG_RT_SOFTINT_OPTIMIZATION
 /*
@@ -1494,75 +1481,6 @@ task_may_not_preempt(struct task_struct *task, int cpu)
 #endif /* CONFIG_RT_SOFTINT_OPTIMIZATION */
 
 static int
-select_task_rq_rt_pixel(struct task_struct *p, int prev_cpu, int sd_flag,
-			     int wake_flags)
-{
-	struct task_struct *curr, *tgt_task;
-	struct rq *rq;
-	struct rq *this_cpu_rq;
-	int target = -1;
-	bool sync = !!(wake_flags & WF_SYNC);
-	int this_cpu;
-	bool sync_wakeup = false;
-	struct cpumask backup_mask;
-	int i;
-	int new_cpu = prev_cpu;
-
-	if (sd_flag != SD_BALANCE_WAKE && sd_flag != SD_BALANCE_FORK)
-		goto out;
-
-	rq = cpu_rq(prev_cpu);
-
-	rcu_read_lock();
-	curr = READ_ONCE(rq->curr);
-	this_cpu = smp_processor_id();
-	this_cpu_rq = cpu_rq(this_cpu);
-
-	/*
-	 * Respect the sync flag as long as the task can run on this CPU.
-	 */
-	if (should_honor_rt_sync(this_cpu_rq, p, sync) &&
-	    cpumask_test_cpu(this_cpu, p->cpus_ptr) &&
-	    rt_task_fits_capacity(p, this_cpu)) {
-		new_cpu = this_cpu;
-		sync_wakeup = true;
-		goto out_unlock;
-	}
-
-	target = find_lowest_rq_pixel(p, &backup_mask);
-
-	if (target != -1) {
-		tgt_task = READ_ONCE(cpu_rq(target)->curr);
-		if (task_may_not_preempt(tgt_task, target) ||
-			p->prio >= cpu_rq(target)->rt.highest_prio.curr) {
-			target = -1;
-
-			for_each_cpu(i, &backup_mask) {
-				tgt_task = READ_ONCE(cpu_rq(i)->curr);
-				if (task_may_not_preempt(tgt_task, i) ||
-					p->prio >= cpu_rq(i)->rt.highest_prio.curr) {
-					continue;
-				} else {
-					target = i;
-					break;
-				}
-			}
-		}
-	}
-
-	if (target != -1 &&
-	   (p->prio < cpu_rq(target)->rt.highest_prio.curr ||
-	   task_may_not_preempt(curr, prev_cpu))) {
-		new_cpu = target;
-	}
-
-out_unlock:
-	rcu_read_unlock();
-out:
-	return new_cpu;
-}
-
-static int
 select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 {
 	struct task_struct *curr, *tgt_task;
@@ -1576,10 +1494,6 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 
 	trace_android_rvh_select_task_rq_rt(p, cpu, flags & 0xF,
 					    flags, &target_cpu);
-
-	/* Add a hook for pixel mod */
-	target_cpu = select_task_rq_rt_pixel(p, cpu, flags & 0xF,
-		    flags);
 
 	if (target_cpu >= 0)
 		return target_cpu;
@@ -1722,122 +1636,6 @@ static int balance_rt(struct rq *rq, struct task_struct *p, struct rq_flags *rf)
 }
 #endif /* CONFIG_SMP */
 
-static int find_least_loaded_cpu(struct task_struct *p, struct cpumask *lowest_mask,
-				 struct cpumask *backup_mask)
-{
-	int cpu, best_cpu = -1, unimportant_best_cpu = -1, least_used_best_cpu = -1;
-	unsigned long min_cpu_util = ULONG_MAX;
-	unsigned long min_unimportant_cpu_util = ULONG_MAX;
-	unsigned long min_cpu_capacity = ULONG_MAX;
-	unsigned int min_exit_lat = UINT_MAX;
-	bool check_cpu_overutilized = true;
-	int prev_cpu = task_cpu(p);
-	unsigned long min_importance = ULONG_MAX;
-
-	cpumask_clear(backup_mask);
-
-	if (cpumask_weight(lowest_mask) == 1)
-		return cpumask_first(lowest_mask);
-
-	rcu_read_lock();
-redo:
-	for_each_cpu(cpu, lowest_mask) {
-		struct cpuidle_state *idle;
-		unsigned long util;
-		unsigned int exit_lat = 0;
-		unsigned long capacity = capacity_orig_of(cpu) - thermal_load_avg(cpu_rq(cpu));
-		unsigned long cpu_importance = READ_ONCE(cpu_rq(cpu)->uclamp[UCLAMP_MIN].value) +
-					       READ_ONCE(cpu_rq(cpu)->uclamp[UCLAMP_MAX].value);
-
-		if (cpu_is_idle(cpu)) {
-			util = 0;
-			idle = idle_get_state(cpu_rq(cpu));
-
-			if (idle)
-				exit_lat = idle->exit_latency;
-
-			if (sched_cpu_idle(cpu)) {
-				exit_lat = 0;
-				cpu_importance = 0;
-			}
-		} else {
-			util = cpu_util(cpu) + cpu_util_rt(cpu_rq(cpu));
-		}
-
-		/* select non-idle cpus without important tasks first */
-		if (exit_lat == 0 && cpu_importance < DEFAULT_IMPRATANCE_THRESHOLD) {
-			cpumask_set_cpu(cpu, backup_mask);
-
-			/* Always prefer the least important cpu. */
-			if (min_importance < cpu_importance)
-				continue;
-
-			/* If importance is the same, choose the least loaded cpu. */
-			if (min_importance == cpu_importance)
-				if (min_unimportant_cpu_util < util)
-					continue;
-
-			min_importance = cpu_importance;
-			unimportant_best_cpu = cpu;
-			min_unimportant_cpu_util = util;
-			continue;
-		}
-
-		if (check_cpu_overutilized &&
-		    cpu_overutilized(uclamp_rq_util_with(cpu_rq(cpu), util, p), capacity, cpu))
-			continue;
-
-		/* Always prefer the least loaded cpu. */
-		if (util > min_cpu_util)
-			continue;
-
-		/* If util is the same: */
-		if (util == min_cpu_util) {
-			/* Prefer lower exit latency. */
-			if (exit_lat > min_exit_lat)
-				continue;
-			/* If exit latency is the same: */
-			if (exit_lat == min_exit_lat) {
-				/* Prefer lower capacity. */
-				if (capacity > min_cpu_capacity )
-					continue;
-				/* If capacity is the same, prefer prev cpu */
-				if (least_used_best_cpu == prev_cpu)
-					continue;
-			}
-		}
-
-		min_cpu_util = util;
-		min_cpu_capacity = capacity;
-		min_exit_lat = exit_lat;
-		least_used_best_cpu = cpu;
-	}
-
-	/* If there is only 1 or 0 non-important cpu, try to find least_used_best_cpu. */
-	if (cpumask_weight(backup_mask) < 2 &&
-		least_used_best_cpu == -1 && check_cpu_overutilized) {
-		check_cpu_overutilized = false;
-		goto redo;
-	}
-
-	rcu_read_unlock();
-
-	if (unimportant_best_cpu != -1) {
-		best_cpu = unimportant_best_cpu;
-		/* backup_mask always contains unimportant_best_cpu.  */
-		cpumask_clear_cpu(unimportant_best_cpu, backup_mask);
-
-		/* No other backup, use least_used_best_cpu as backup. */
-		if (cpumask_empty(backup_mask) && least_used_best_cpu != -1)
-			cpumask_set_cpu(least_used_best_cpu, backup_mask);
-	} else if (least_used_best_cpu != -1) {
-		/* Fall back to least_used_best_cpu if there is no non-important cpu. */
-		best_cpu = least_used_best_cpu;
-	}
-
-	return best_cpu;
-}
-
 /*
  * Preempt the current task with a newly woken task if needed:
  */
@@ -1979,69 +1777,6 @@ static struct task_struct *pick_highest_pushable_task(struct rq *rq, int cpu)
 }
 
 static DEFINE_PER_CPU(cpumask_var_t, local_cpu_mask);
-
-static int find_lowest_rq_pixel(struct task_struct *p, struct cpumask *backup_mask)
-{
-	struct sched_domain *sd;
-	struct cpumask lowest_mask;
-	int this_cpu = smp_processor_id();
-	int cpu;
-	int ret;
-
-	if (p->nr_cpus_allowed == 1) {
-		return cpumask_first(p->cpus_ptr);
-	}
-
-	ret = cpupri_find_fitness(&task_rq(p)->rd->cpupri, p, &lowest_mask, rt_task_fits_capacity);
-	if (!ret) {
-		return -1;
-	}
-
-	cpu = find_least_loaded_cpu(p, &lowest_mask, backup_mask);
-	if (cpu != -1) {
-		return cpu;
-	}
-
-	cpu = task_cpu(p);
-	if (cpumask_test_cpu(cpu, &lowest_mask)) {
-		return cpu;
-	}
-
-	if (!cpumask_test_cpu(this_cpu, &lowest_mask))
-		this_cpu = -1;
-
-	rcu_read_lock();
-	for_each_domain(cpu, sd) {
-		if (sd->flags & SD_WAKE_AFFINE) {
-			int best_cpu;
-
-			if (this_cpu != -1 &&
-			    cpumask_test_cpu(this_cpu, sched_domain_span(sd))) {
-				rcu_read_unlock();
-				return this_cpu;
-			}
-
-			best_cpu = cpumask_first_and(&lowest_mask,
-						     sched_domain_span(sd));
-			if (best_cpu < nr_cpu_ids) {
-				rcu_read_unlock();
-				return best_cpu;
-			}
-		}
-	}
-	rcu_read_unlock();
-
-	if (this_cpu != -1) {
-		return this_cpu;
-	}
-
-	cpu = cpumask_any(&lowest_mask);
-	if (cpu < nr_cpu_ids) {
-		return cpu;
-	}
-
-	return -1;
-}
 
 static int find_lowest_rq(struct task_struct *task)
 {
